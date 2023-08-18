a7456e03e2ea4a738b61642642b5491bbdcb583f
amin-bani-a <amin.baniamerian67@gmail.com>
2023-08-18 11:29:47 +0400

    setup random functionality and fetch

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,41 @@ function App() {
   const [title, setTitle] = useState("name");
   const [value, setValue] = useState("random person");
 
+  const getPerson = async () => {
+    const response = await fetch(url);
+    const data = await response.json();
+    const person = data.results[0];
+    const { phone, email } = person;
+    const { large: image } = person.picture;
+    const {
+      login: { password },
+    } = person;
+    const { first, last } = person.name;
+    const {
+      dob: { age },
+    } = person;
+    const {
+      street: { number, name },
+    } = person.location;
+    const newPerson = {
+      image,
+      phone,
+      email,
+      password,
+      age,
+      street: `${number} ${name}`,
+      name: `${first} ${last}`,
+    };
+    setPerson(newPerson);
+    setLoading(false);
+    setTitle("name");
+    setValue(newPerson.name);
+  };
+
+  useEffect(() => {
+    getPerson();
+  }, []);
+
   const handleValue = (e) => {
     console.log(e.target);
   };
@@ -25,7 +60,7 @@ function App() {
       <div className="block">
         <div className="container">
           <img
-            src={(person && person.img) || defaultImage}
+            src={(person && person.image) || defaultImage}
             alt="random user"
             className="user-img"
           />
@@ -34,48 +69,44 @@ function App() {
           <div className="values-list">
             <button
               className="icon"
-              data-aria-label="name"
+              data-label="name"
               onMouseOver={handleValue}
             >
               <FaUser />
             </button>
             <button
               className="icon"
-              data-aria-label="email"
+              data-label="email"
               onMouseOver={handleValue}
             >
               <FaEnvelopeOpen />
             </button>
-            <button
-              className="icon"
-              data-aria-label="age"
-              onMouseOver={handleValue}
-            >
+            <button className="icon" data-label="age" onMouseOver={handleValue}>
               <FaCalendarTimes />
             </button>
             <button
               className="icon"
-              data-aria-label="street"
+              data-label="street"
               onMouseOver={handleValue}
             >
               <FaMap />
             </button>
             <button
               className="icon"
-              data-aria-label="phone"
+              data-label="phone"
               onMouseOver={handleValue}
             >
               <FaPhone />
             </button>
             <button
               className="icon"
-              data-aria-label="password"
+              data-label="password"
               onMouseOver={handleValue}
             >
               <FaLock />
             </button>
           </div>
-          <button className="btn" type="button">
+          <button className="btn" type="button" onClick={getPerson}>
             {loading ? "Loading..." : "random user"}
           </button>
         </div>
